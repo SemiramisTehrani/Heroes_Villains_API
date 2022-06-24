521f8ce382ee0d98f4dd9b7cf6d29475174e2488
unknown <semtehrani@yahoo.com>
2022-06-23 22:34:02 -0700

    messed up the file names so deleted all of my work to go with supers file name

--- heroes_villains/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HeroesVillainsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'heroes_villains'
--- heroes_villains/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Heroes_Villains
-
-
-class Heroes_VillainsSerializer(serializers.ModelSerializer):
-    class Meta :
-        model = Heroes_Villains
-        fields = ['id','name','alter_ego','primary_ability','secondary_ability','catchphrase']
