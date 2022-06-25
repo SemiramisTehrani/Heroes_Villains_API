2bbc0c9df7babd1025a93d6f957863a8ccf3615c
unknown <semtehrani@yahoo.com>
2022-06-25 07:18:01 -0700

    Postman worked to show ok but not showing supers_list

--- supers/views.py
+++ supers/views.py
@@ -1,8 +1,15 @@
+
+# from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
+
 
 @api_view(['GET'])
 def supers_list(request): 
 
-    return Response('ok')
+    supers = Super.objects.all()
+    serializer = SuperSerializer(supers, many = True)
+
+    return Response(serializer.data)
